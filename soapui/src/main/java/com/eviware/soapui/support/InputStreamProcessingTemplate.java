7e97efd791c990f18e75ea1503d549b94c095480
Marc Paquette <marcpa@mac.com>
2013-12-06 15:36:10 -0500

    Use a ‘Template Method’ pattern to keep the IO exception handling in one place, based on http://tutorials.jenkov.com/java-exception-handling/exception-handling-templates.html.

---
+++ soapui/src/main/java/com/eviware/soapui/support/InputStreamProcessingTemplate.java
@@ -0,0 +1,41 @@
+package com.eviware.soapui.support;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Class that encapsulates exception handling for file IO.
+ * From http://tutorials.jenkov.com/java-exception-handling/exception-handling-templates.html
+ */
+public class InputStreamProcessingTemplate {
+
+    public static void process(String fileName,
+                               InputStreamProcessor processor) throws Exception {
+        IOException processException = null;
+        InputStream input = null;
+        try{
+            input = new FileInputStream(fileName);
+
+            processor.process(input);
+        } catch (IOException e) {
+            processException = e;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    if (processException != null) {
+                        Exception wrappedException = new Exception("IO Exception thrown from processor.process()", processException);
+                        throw new Exception(e.getMessage() + fileName, wrappedException);
+                    } else {
+                        throw new Exception("Error closing InputStream for file " + fileName, e);
+                    }
+                }
+            }
+            if (processException != null) {
+                throw new Exception("Error processing InputStream for file " + fileName, processException);
+            }
+        }
+    }
+}
