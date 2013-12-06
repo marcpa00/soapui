e9a4dde302ef11630c7bf24beb3565595eaaad0d
Marc Paquette <marcpa@mac.com>
2013-12-06 15:36:10 -0500

    Use a ‘Template Method’ pattern to keep the IO exception handling in one place, based on http://tutorials.jenkov.com/java-exception-handling/exception-handling-templates.html.

---
+++ soapui/src/main/java/com/eviware/soapui/support/InputStreamProcessor.java
@@ -0,0 +1,12 @@
+package com.eviware.soapui.support;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Interface to implements when using the InputStreamProcessingTemplate.
+ * From http://tutorials.jenkov.com/java-exception-handling/exception-handling-templates.html
+ */
+public interface InputStreamProcessor {
+    public void process(InputStream input) throws IOException;
+    public String getResult();
+}
